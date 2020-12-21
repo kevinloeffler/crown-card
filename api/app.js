1003949230187a7f4c5f67f27d575eda7aec4912
Kevin Loeffler <hi@kevinloeffler.ch>
2020-12-21 14:31:15 +0100

    Added Page 'testAPI'

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testAPIRouter = require('./routes/testAPI');
 
 var app = express();
 
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
