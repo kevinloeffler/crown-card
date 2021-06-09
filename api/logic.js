1d8a20cc62c887d0eb1df561ac024469ddd01c6d
Kevin Loeffler <hi@kevinloeffler.ch>
2021-06-09 23:56:26 +0200

    Built authenticate, validateCard, chargeMoney and addMoney functions

--- api/logic.js
+++ api/logic.js
@@ -1,5 +1,28 @@
 function authenticate (key) {
-    console.log(key)
+    if (key === process.env.AUTH_TOKEN) {
+        console.log('Login successful')
+        return true
+    }
+    console.log('Login denied')
+    return false
 }
 
-export {authenticate}
+function validateCard (cardID) {
+    // ToDo: check if card is valid
+    console.log('Valid Card ID')
+    return true
+}
+
+function chargeMoney (amount) {
+    //ToDo: Check if amount is integer
+    //ToDo: Check if chard has enough money
+    //ToDo: Server Request
+    return true
+}
+
+function addMoney (amount) {
+    //ToDo: Add money to card
+    //ToDo: Server Request
+}
+
+export {authenticate, validateCard, chargeMoney, addMoney}
