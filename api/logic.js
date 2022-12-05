989631b842439b6799df2ab6ef24d396ed2f3021
Kevin Loeffler <hi@kevinloeffler.ch>
2022-12-05 22:29:01 +0100

    ADD: password to db authentication

--- api/logic.js
+++ api/logic.js
@@ -10,7 +10,7 @@ dotenv.config()
 const pool = new Pool ({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    //password: ''
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     max: 20,
